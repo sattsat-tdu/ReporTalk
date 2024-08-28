81435f0ca617f18970ed8cc365e4414117fcec8f
sattsat <syuuatit141offer@gmail.com>
2024-08-29 01:02:22 +0900

    first commit

---
+++ App/ReportChat/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ReportChat
+//  
+//  Created by SATTSAT on 2024/08/29
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ App/ReportChat/ReportChatApp.swift
@@ -0,0 +1,18 @@
+//
+//  ReportChatApp.swift
+//  ReportChat
+//  
+//  Created by SATTSAT on 2024/08/29
+//  
+//
+
+import SwiftUI
+
+@main
+struct ReportChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
