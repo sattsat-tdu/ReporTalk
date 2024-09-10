94f7333cc9b00d77de14e6c6f208731aa5bd57bf
sattsat <syuuatit141offer@gmail.com>
2024-09-11 00:32:28 +0900

    ログイン処理を実装

---
+++ App/ReportChat/Screens/Login/LoginViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewModel.swift
+//  ReportChat
+//  
+//  Created by SATTSAT on 2024/09/01
+//  
+//
+
+import Foundation
+
+@MainActor
+final class LoginViewModel: ObservableObject {
+    
+    @Published var id = ""
+    @Published var password = ""
+    @Published var errorMessage = ""
+    
+    @Published var alertType: AlertType? = nil
+    
+    func login() {
+        Task {
+            errorMessage = ""
+            let loginResult = await AuthModel.shared.login(id: id, password: password)
+            switch loginResult {
+            case .success(let response):
+                print(response.user)
+            case .failure(let loginError):
+                errorMessage = FirebaseError.shared.getErrorMessage(loginError)
+//                alertType = AlertType(title: "エラー", message: errorMessage)
+                //↓ダイアログ表示、今後実装
+//                FirebaseError.shared.handle(loginError)
+            }
+        }
+    }
+}
