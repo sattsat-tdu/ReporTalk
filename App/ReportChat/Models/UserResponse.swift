d9cf8e5ba3eb99c606af33f6991feaa99f809f0d
sattsat <syuuatit141offer@gmail.com>
2024-09-16 23:04:31 +0900

    ユーザーレスポンスのinit処理を実装

--- App/ReportChat/Models/UserResponse.swift
+++ App/ReportChat/Models/UserResponse.swift
@@ -12,7 +12,16 @@ struct UserResponse: Decodable {
     let id: String
     let userName: String
     let email: String
-    let friends: [UserResponse]
+    let friends: [String]
     let photoURL: String?
-    let rooms: [RoomResponse]
+    let rooms: [String]
+    
+    init(data: [String: Any]) {
+        self.id = data["uid"] as? String ?? ""
+        self.userName = data["displayName"] as? String ?? "nilName"
+        self.email = data["email"] as? String ?? ""
+        self.friends = data["friends"] as? [String] ?? []
+        self.photoURL = data["photoURL"] as? String
+        self.rooms = data["rooms"] as? [String] ?? []
+    }
 }
