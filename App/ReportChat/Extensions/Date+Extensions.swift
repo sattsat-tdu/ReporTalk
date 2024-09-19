1a9be9e2ffd2618eb5bcef7078eded566556bf7c
sattsat <syuuatit141offer@gmail.com>
2024-09-19 17:23:30 +0900

    Date型とUIImage型のExtensionファイルを追加

---
+++ App/ReportChat/Extensions/Date+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Date+Extension.swift
+//  ReportChat
+//  
+//  Created by SATTSAT on 2024/09/19
+//  
+//
+
+import SwiftUI
+
+extension Date {
+    func toText() -> String {
+        return "昨日"
+    }
+}
