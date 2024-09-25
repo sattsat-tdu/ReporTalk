b3c62f8db5f8b6a5c9825f4576b92feb4612784b
sattsat <syuuatit141offer@gmail.com>
2024-09-25 23:08:04 +0900

    設定構造モデルを使用したViewの構成

--- App/ReportChat/Screens/MyPage/MyPageView.swift
+++ App/ReportChat/Screens/MyPage/MyPageView.swift
@@ -7,29 +7,69 @@
 //
 
 import SwiftUI
+import SwiftUIFontIcon
 
 struct MyPageView: View {
     
     let user: UserResponse
     @ObservedObject var viewModel = MyPageViewModel()
-    
+
     var body: some View {
-        VStack {
-            Text("ログイン情報")
-                .font(.headline)
-            
-            Text(user.userName)
-                .font(.headline)
-            
-            Text(user.id ?? "nilID")
-            
-            Divider()
-            
-            ForEach(user.rooms, id: \.self) { room in
-                Text(room)
+        List {
+            VStack {
+                Text("プロフィール...")
             }
+            .frame(height: 100)
+            
+            CustomNavCell(
+                navItem: NavItem(
+                    destination: AnyView(SettingsView(section: settingResource)),
+                    icon: .settings,
+                    title: "設定"))
+            .listRowSeparator(.hidden)
+            .padding(.vertical, 4)
+            .frame(minHeight: 38)
+            
+            SafariCell(
+                safariItem: SafariItem(
+                    title: "お問い合わせ・要望",
+                    icon: .question_answer,
+                    url: "https://sattsat.blogspot.com/2021/05/sattsat-sattsat-sattsat-admobgoogle-inc.html?m=1"
+                ))
+            .listRowSeparator(.hidden)
+            .padding(.vertical, 4)
+            .frame(minHeight: 38)
+            
+            VStack {
+                Text("AdMob...")
+            }
+            .frame(height: 80)
+            
+            SafariCell(
+                safariItem: SafariItem(
+                    title: "コピーライト",
+                    icon: .content_paste,
+                    url: "https://sattsat.blogspot.com/2022/07/blog-post.html?m=1"
+                ))
+            .listRowSeparator(.hidden)
+            .padding(.vertical, 4)
+            .frame(minHeight: 38)
+            
+            SafariCell(
+                safariItem: SafariItem(
+                    title: "プライバシーポリシー",
+                    icon: .verified_user,
+                    url: "https://sattsat.blogspot.com/2021/05/sattsat-sattsat-sattsat-admobgoogle-inc.html?m=1"
+                ))
+            .listRowSeparator(.hidden)
+            .padding(.vertical, 4)
+            .frame(minHeight: 38)
+            
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .listRowSpacing(10)
+        .scrollContentBackground(.hidden)
+        .navigationTitle("マイページ")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
