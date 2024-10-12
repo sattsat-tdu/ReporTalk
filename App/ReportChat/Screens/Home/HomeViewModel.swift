867b68c61b7bf1838e7cb5f287364cbf78d6a5e7
sattsat <syuuatit141offer@gmail.com>
2024-10-12 13:01:48 +0900

    ホームにViewModelを追加

---
+++ App/ReportChat/Screens/Home/HomeViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewModel.swift
+//  ReportChat
+//  
+//  Created by SATTSAT on 2024/10/12
+//  
+//
+
+import Foundation
+
+@MainActor
+final class HomeModel: ObservableObject {
+    
+}
