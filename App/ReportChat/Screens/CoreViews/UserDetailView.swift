b84982b1b1869b2925831968718085fcebb6df1d
sattsat <syuuatit141offer@gmail.com>
2024-10-15 11:38:33 +0900

    ディレクトリの更新とユーザープロフィールViewの作成

---
+++ App/ReportChat/Screens/CoreViews/UserDetailView.swift
@@ -0,0 +1,19 @@
+//
+//  UserDetailsView.swift
+//  ReportChat
+//  
+//  Created by SATTSAT on 2024/10/15
+//  
+//
+
+import SwiftUI
+
+struct UserDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDetailView()
+}
