198c36b5e59c3cb974889e7865256adf78f27cb8
sattsat <syuuatit141offer@gmail.com>
2024-10-15 22:40:29 +0900

    ルーム情報をFireStoreに書き込むための変換処理を実装

--- App/ReportChat/Models/RoomResponse.swift
+++ App/ReportChat/Models/RoomResponse.swift
@@ -23,4 +23,20 @@ struct RoomResponse: Decodable {
         case roomIcon = "roomicon"
         case roomName = "roomname"
     }
+    
+    // Firebaseに書き込むための辞書変換
+    func toDictionary() -> [String: Any] {
+        var dict: [String: Any] = [
+            "members": members
+        ]
+        
+        if let roomIcon = self.roomIcon {
+            dict["roomicon"] = roomIcon
+        }
+        
+        if let roomName = self.roomName {
+            dict["roomname"] = roomName
+        }
+        return dict
+    }
 }
