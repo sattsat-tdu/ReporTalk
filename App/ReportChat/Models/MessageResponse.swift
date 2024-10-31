eb25a836d69ff6e456cac2b9f366efd73fe1fe46
sattsat <syuuatit141offer@gmail.com>
2024-10-31 11:46:42 +0900

    レポータグ実装のため、メッセージモデルの要素を追加

--- App/ReportChat/Models/MessageResponse.swift
+++ App/ReportChat/Models/MessageResponse.swift
@@ -8,12 +8,18 @@
 
 import Foundation
 import FirebaseFirestore
+import SwiftUI
 
 struct MessageResponse: Decodable {
     @DocumentID var id: String?
     let text: String
     let senderId: String
     let timestamp: Date
+    let reportag: String
+    
+    func toReportag() -> Reportag? {
+        return Reportag(rawValue: reportag)
+    }
     
     //Firebaseのフィールド名と一致させる
     enum CodingKeys: String, CodingKey {
@@ -21,6 +27,7 @@ struct MessageResponse: Decodable {
         case text = "content"
         case senderId
         case timestamp
+        case reportag
     }
     
     // Firebaseに書き込むための辞書変換
@@ -28,7 +35,62 @@ struct MessageResponse: Decodable {
         return [
             "content": text,
             "senderId": senderId,
-            "timestamp": Timestamp(date: timestamp)  // DateをTimestampに変換
+            "timestamp": timestamp,  // DateをTimestampに変換
+            "repotag": reportag
         ]
     }
 }
+
+enum Reportag: String, Codable {
+    case goodNews = "good_news"
+    case badNews = "bad_news"
+    case dailyReport = "daily_report"
+    case breaking = "breaking"
+    case announcement = "announcement"
+    case question = "question"
+    case important = "important"
+    case emptiness = "emptiness"
+    
+    var tagName: String {
+        switch self {
+        case .goodNews:
+            return "朗報"
+        case .badNews:
+            return "悲報"
+        case .dailyReport:
+            return "日報"
+        case .breaking:
+            return "速報"
+        case .announcement:
+            return "告知"
+        case .question:
+            return "質問"
+        case .important:
+            return "重要"
+        case .emptiness:
+            return "虚無"
+        }
+    }
+    
+    var color: Color {
+        switch self {
+        case .goodNews:
+            return .goodNews
+        case .badNews:
+            return .badNews
+        case .dailyReport:
+            return .dailyReport
+        case .breaking:
+            return .breaking
+        case .announcement:
+            return .announcement
+        case .question:
+            return .question
+        case .important:
+            return .important
+        case .emptiness:
+            return .emptiness
+        }
+    }
+}
+
