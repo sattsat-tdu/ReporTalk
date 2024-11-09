5bf66b36c7fa2b1fe7ca4441a0fe0f670a444a06
sattsat <syuuatit141offer@gmail.com>
2024-11-09 15:35:40 +0900

    各メッセージの表示形式を、タグ付きの場合はタグカラーに設定し、通常時は普段の背景色にするように構成

--- App/ReportChat/ViewParts/MessageCell.swift
+++ App/ReportChat/ViewParts/MessageCell.swift
@@ -1,31 +1,65 @@
 //
 //  MessageView.swift
 //  ReportChat
-//  
+//
 //  Created by SATTSAT on 2024/09/19
-//  
+//
 //
 
 import SwiftUI
+import SwiftUIFontIcon
 
 struct MessageCell: View {
     
     let message: MessageResponse
+    var reportag: Reportag? {
+        return message.toReportag()
+    }
     let isCurrentUser: Bool
     private let cornerRadius:CGFloat = 8
     
     var body: some View {
         VStack(alignment: isCurrentUser ? .trailing : .leading) {
-            Text(message.text)
-                .foregroundStyle(.primary)
-                .padding()
-                .background(isCurrentUser ? .sendMessage : .receivedMessage)
-                .clipShape(.rect(
-                    topLeadingRadius: cornerRadius,
-                    bottomLeadingRadius: isCurrentUser ? cornerRadius : 0,
-                    bottomTrailingRadius: isCurrentUser ? 0 : cornerRadius,
-                    topTrailingRadius: cornerRadius
-                ))
+            
+            VStack( alignment: isCurrentUser ? .trailing : .leading ) {
+                if let reportag = reportag {
+                    HStack {
+                        FontIcon.text(.materialIcon(code: .insert_emoticon))
+                        
+                        Text(reportag.tagName)
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .padding(.leading, 4)
+                    }
+                    .padding(8)
+                    .background(reportag.color.gradient)
+                    .clipShape(.rect(cornerRadius: 8))
+                    .padding(.bottom, 4)
+                }
+                
+                Text(message.text)
+                    .font(.body)
+            }
+            .padding(10)
+            .background(
+                Group {
+                    if let reportag = reportag {
+                        Color.back
+                            .overlay {
+                                reportag.color.opacity(0.4)
+                            }
+                    } else {
+                        isCurrentUser ?
+                        Color.sendMessage : Color.receivedMessage
+                    }
+                }
+            )
+            .clipShape(.rect(
+                topLeadingRadius: isCurrentUser ? cornerRadius : 0,
+                bottomLeadingRadius: cornerRadius,
+                bottomTrailingRadius: cornerRadius,
+                topTrailingRadius: isCurrentUser ? 0 : cornerRadius
+            ))
             
             Text(message.timestamp.toString())
                 .font(.caption2)
@@ -36,12 +70,12 @@ struct MessageCell: View {
 }
 
 #Preview {
-    MessageCell(message: 
+    MessageCell(message:
                     MessageResponse(
                         text: "サンプルテキスト",
                         senderId: "id1",
                         timestamp: Date(),
-                        reportag: Reportag.goodNews.rawValue),
+                        reportag: Reportag.badNews.rawValue),
                 isCurrentUser: true
     )
 }
