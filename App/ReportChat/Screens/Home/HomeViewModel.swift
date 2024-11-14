ea73e3a170bea1984952d500b13ccb54974d76be
sattsat <syuuatit141offer@gmail.com>
2024-11-14 20:58:10 +0900

    ホームに円グラフを追加し、レポータグを可視化

--- App/ReportChat/Screens/Home/HomeViewModel.swift
+++ App/ReportChat/Screens/Home/HomeViewModel.swift
@@ -6,9 +6,36 @@
 //  
 //
 
-import Foundation
+import SwiftUI
+import SwiftData
 
 @MainActor
-final class HomeModel: ObservableObject {
+final class HomeViewModel: ObservableObject {
+    @Published var tagCounts: [Reportag: Int] = [:]  // タグごとの件数
+    @Published var hasData = false
     
+    init() {
+        fetchReporTagCounts()
+    }
+    
+    func fetchReporTagCounts() {
+        
+        guard let userId = AppManager.shared.currentUser?.id else {return}
+        
+        let messages = SwiftDataManager.shared.fetchData(userId: userId)
+        
+        // データがあるかどうかを判定
+        self.hasData = !messages.isEmpty
+        
+        // タグごとにメッセージの件数をカウント
+        let counts = Dictionary(grouping: messages, by: { $0.reportag }).mapValues { $0.count }
+        
+        // EnumのReportagと一致させてtagCountsに変換
+        var tagCountDict: [Reportag: Int] = [:]
+        for tag in Reportag.allCases {
+            tagCountDict[tag] = counts[tag.rawValue] ?? 0  // データがないタグは0
+        }
+        
+        self.tagCounts = tagCountDict
+    }
 }
