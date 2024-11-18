dbfd29cb57ddaf60ef7f7c82d64ec567570ab44e
sattsat <syuuatit141offer@gmail.com>
2024-11-19 01:51:27 +0900

    アイコン選択時に正方形切り抜きを行うコードの追加を行い、初期設定画面を更新

--- App/ReportChat/ViewParts/PhotoPickerView.swift
+++ App/ReportChat/ViewParts/PhotoPickerView.swift
@@ -1,55 +1,53 @@
 //
-//  PhotoPickerView.swift
+//  PhotoPickerView2.swift
 //  ReportChat
 //  
-//  Created by SATTSAT on 2024/09/30
+//  Created by SATTSAT on 2024/11/19
 //  
 //
 
 import SwiftUI
-import PhotosUI
-import SwiftUIFontIcon
 
-struct PhotoPickerView: View {
-    
-    @State var selectedPhoto: PhotosPickerItem?
-    @Binding var selectedImageData: Data?
-    
-    var body: some View {
-        PhotosPicker(selection: $selectedPhoto,
-                     matching: .images,
-                     photoLibrary: .shared()
-        ){
-            if let imageData = selectedImageData,
-               let uiImage = UIImage(data: imageData){
-                Image(uiImage: uiImage)
-                    .resizable()
-                    .scaledToFit()
-                    .clipShape(Circle())
-                    .frame(height: 80)
-                    .shadow(radius: 3)
-            } else {
-                FontIcon.text(.materialIcon(code: .account_circle), fontsize: 80)
-                    .foregroundStyle(.fieldBack)
-            }
+public struct PhotoPickerView: UIViewControllerRepresentable {
+    @Environment(\.dismiss) private var dismiss: DismissAction
+
+    @Binding private var imageData: Data?
+
+    public init(imageData: Binding<Data?>) {
+        self._imageData = imageData
+    }
+
+    public func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    public func makeUIViewController(context: Context) -> UIImagePickerController {
+        let uiImagePickerController = UIImagePickerController()
+        uiImagePickerController.delegate = context.coordinator
+        uiImagePickerController.sourceType = .photoLibrary
+        uiImagePickerController.allowsEditing = true
+        return uiImagePickerController
+    }
+
+    public func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    public class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: PhotoPickerView
+
+        public init(_ parent: PhotoPickerView) {
+            self.parent = parent
         }
-        .onChange(of: selectedPhoto) {
-            if let selectedImageData = selectedPhoto {
-                selectedImageData.loadTransferable(type: Data.self) { result in
-                    switch result {
-                    case .success(let data):
-                        if let data = data {
-                            self.selectedImageData = data
-                        }
-                    case .failure:
-                        return
-                    }
-                }
+
+        public func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]
+        ) {
+            if let image = info[.editedImage] as? UIImage {
+                parent.imageData = image.jpegData(compressionQuality: 0.8) // Convert UIImage to Data
+            } else if let image = info[.originalImage] as? UIImage {
+                parent.imageData = image.jpegData(compressionQuality: 0.8) // Convert UIImage to Data
             }
+            parent.dismiss()
         }
     }
-}
-
-#Preview {
-    PhotoPickerView(selectedImageData: .constant(Data()))
 }
