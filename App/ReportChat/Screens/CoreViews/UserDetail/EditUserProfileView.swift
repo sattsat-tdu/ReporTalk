5c834fde3daf22e456519bdb43a29f3325847db3
sattsat <syuuatit141offer@gmail.com>
2024-11-23 13:07:14 +0900

    ユーザー情報を変更するViewの作成

---
+++ App/ReportChat/Screens/CoreViews/UserDetail/EditUserProfileView.swift
@@ -0,0 +1,19 @@
+//
+//  EditUserProfileView.swift
+//  ReportChat
+//  
+//  Created by SATTSAT on 2024/11/23
+//  
+//
+
+import SwiftUI
+
+struct EditUserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditUserProfileView()
+}
