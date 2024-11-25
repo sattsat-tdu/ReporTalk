8756719740a423456f3d21f3b79e6e0d28fdc3fb
sattsat <syuuatit141offer@gmail.com>
2024-11-26 00:21:05 +0900

    handle nameを変更するViewを作成

---
+++ App/ReportChat/ViewParts/SettingViews/HandlenameSettingView.swift
@@ -0,0 +1,19 @@
+//
+//  HandlenameSettingView.swift
+//  ReportChat
+//  
+//  Created by SATTSAT on 2024/11/26
+//  
+//
+
+import SwiftUI
+
+struct HandlenameSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HandlenameSettingView()
+}
