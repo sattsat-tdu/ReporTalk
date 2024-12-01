0f8972ba412728cab4f42d0ff72819c9d293e3b5
sattsat <syuuatit141offer@gmail.com>
2024-12-02 00:24:29 +0900

    レポータ一覧をグリッド表示するViewの作成

---
+++ App/ReportChat/ViewParts/ReportansBookView.swift
@@ -0,0 +1,19 @@
+//
+//  ReportansBookView.swift
+//  ReportChat
+//  
+//  Created by SATTSAT on 2024/12/02
+//  
+//
+
+import SwiftUI
+
+struct ReportansBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReportansBookView()
+}
